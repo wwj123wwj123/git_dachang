70a9a4400e3c616588c9f8b3ad28de58799604ee
王文杰 <2762999285@qq.com>
2023-06-20 11:36:28 +0800

    初始化

---
+++ src/main/java/com/dachuang/GitDachangApplication.java
@@ -0,0 +1,13 @@
+package com.dachuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDachangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDachangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dachuang/GitDachangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dachuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDachangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
