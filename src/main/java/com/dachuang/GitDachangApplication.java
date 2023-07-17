f871cad24a2ff00e14837468ce58eca1aeaad519
王文杰 <2762999285@qq.com>
2023-07-17 19:46:28 +0800

    两个启动入口

--- src/main/java/com/dachuang/GitDachangApplication.java
+++ src/main/java/com/dachuang/GitDachangApplication.java
@@ -1,9 +1,11 @@
 package com.dachuang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.dachuang.dao")
 public class GitDachangApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/dachuang/GitDachangApplicationTests.java
+++ src/test/java/com/dachuang/GitDachangApplicationTests.java
@@ -1,13 +1,25 @@
 package com.dachuang;
 
+import com.dachuang.dao.UserDao.UserMapper;
+import com.dachuang.domin.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GitDachangApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
     }
-
+    @Test
+    public void selectUserByEmail()
+    {
+       User user = userMapper.selectUserByEmail("2762999285@qq.com");
+        System.out.println(user);
+    }
 }
