f025ef966494f753bbe3deb69430883431479a8a
王文杰 <2762999285@qq.com>
2023-07-17 19:44:25 +0800

    返回Result里面的Code

---
+++ src/main/java/com/dachuang/controller/common/Code.java
@@ -0,0 +1,14 @@
+package com.dachuang.controller.common;
+
+public class Code {
+    public static final Integer SAVE_OK =20011; //保存成功
+    public static final Integer DELETE_OK =20021; //删除成功
+    public static final Integer UPDATE_OK =20031; //更新成功
+    public static final Integer GET_OK =20041; //查询成功
+
+    public static final Integer SAVE_Err =20010; //保存失败
+    public static final Integer DELETE_ERR =20020; //删除失败
+    public static final Integer UPDATE_Err =20030; //更新失败
+    public static final Integer GET_Err =20040; //查询失败
+    public static final Integer SYSTEM_UNKNOW_ERR =59999;//系统未知错误
+}
