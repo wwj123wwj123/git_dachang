61a7cfd4f8f2cccbf1e282e04a12e1c21ddd7258
王文杰 <2762999285@qq.com>
2023-07-18 10:52:52 +0800

    sql测试

--- src/test/java/com/dachuang/GitDachangApplicationTests.java
+++ src/test/java/com/dachuang/GitDachangApplicationTests.java
@@ -22,4 +22,11 @@ class GitDachangApplicationTests {
        User user = userMapper.selectUserByEmail("2762999285@qq.com");
         System.out.println(user);
     }
+    @Test
+    public void registerUser()
+    {
+        User user = new User("wwj","15963@qq.com","123456");
+        userMapper.registerUser(user);
+
+    }
 }
