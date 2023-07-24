f2421f21af7a23139dc4130d9d9fb68e1f9b1612
王文杰 <2762999285@qq.com>
2023-07-24 14:38:39 +0800

    上传和下载接口

---
+++ src/main/java/com/dachuang/controller/file/FileController.java
@@ -0,0 +1,41 @@
+package com.dachuang.controller.file;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import com.dachuang.entity.file.dto.FileDownloadDTO;
+import com.dachuang.entity.file.dto.FileUploadDTO;
+import com.dachuang.service.file.FileService;
+import com.dachuang.util.HttpResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.List;
+
+@RestController
+public class FileController {
+    @Value("${file.location}")
+    private String fileLocation;
+    @Autowired
+    private FileService fileService;
+
+    /**
+     * 上传接口
+     * @param fileUploadDTO
+     * @return
+     */
+    @PostMapping("/upload")
+    public HttpResult upload(FileUploadDTO fileUploadDTO) {
+        return fileService.uploadFile(fileUploadDTO);
+    }
+    /**
+    下载接口
+     */
+    @GetMapping("/download/{flag}")
+    public void getFiles(@PathVariable String flag, HttpServletResponse response)  {
+        fileService.downloadFile(new FileDownloadDTO(flag,response));
+    }
+}
