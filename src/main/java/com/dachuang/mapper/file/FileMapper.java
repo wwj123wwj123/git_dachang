638bf795b6cf7cb3e523a6a7640a5c3e1d2a71a2
王文杰 <2762999285@qq.com>
2023-07-24 14:40:42 +0800

    文件上传和下载的Mapper接口

---
+++ src/main/java/com/dachuang/mapper/file/FileMapper.java
@@ -0,0 +1,10 @@
+package com.dachuang.mapper.file;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dachuang.entity.file.po.FileDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FileMapper extends BaseMapper<FileDO> {
+
+}
