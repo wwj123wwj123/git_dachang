89745fafcf8a0e4d7ee4d244566abc0f68527ec4
王文杰 <2762999285@qq.com>
2023-07-24 14:42:38 +0800

    启动口

--- src/main/java/com/dachuang/GitDachangApplication.java
+++ src/main/java/com/dachuang/GitDachangApplication.java
@@ -1,11 +1,12 @@
 package com.dachuang;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.dachuang.dao")
+@ComponentScan(value = "com.dachuang.*")
 public class GitDachangApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/dachuang/GitDachangApplicationTests.java
+++ src/test/java/com/dachuang/GitDachangApplicationTests.java
@@ -1,7 +1,8 @@
 package com.dachuang;
 
-import com.dachuang.dao.UserDao.UserMapper;
-import com.dachuang.domin.User;
+import cn.hutool.core.io.FileUtil;
+import com.dachuang.entity.user.po.UserDO;
+import com.dachuang.mapper.user.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,21 +13,41 @@ import java.util.List;
 class GitDachangApplicationTests {
     @Autowired
     private UserMapper userMapper;
-
+    @Autowired
+    private UserDO userDO;
     @Test
     void contextLoads() {
     }
     @Test
     public void selectUserByEmail()
     {
-       User user = userMapper.selectUserByEmail("2762999285@qq.com");
-        System.out.println(user);
+        userDO = userMapper.selectUserByEmail("2762999285@qq.com");
+        System.out.println(userDO);
     }
     @Test
     public void registerUser()
     {
-        User user = new User("wwj","15963@qq.com","123456");
-        userMapper.registerUser(user);
-
+        userDO =UserDO.builder()
+                .userName("wwj")
+                .email("15963@qq.com")
+                .password("123456")
+                .build();
+        userMapper.insert(userDO);
+    }
+    @Test
+    public void testUpLoad()
+    {
+        String s = "2023年西安理工大学大创项目重点领域项目范围指南.doc";
+//        String substring = s.substring(s.lastIndexOf('.') + 1);
+//        System.out.println(substring);
+    }
+    @Test
+    public void testDownload()
+    {
+        String basePath ="D:/大创/网站/";
+        List<String> fileNames = FileUtil.listFileNames(basePath);
+        for (String fileName : fileNames) {
+            System.out.println(fileName);
+        }
     }
 }
