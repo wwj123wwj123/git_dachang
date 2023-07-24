a3c048164e687a1a3773f23f82b24b51c4076df1
王文杰 <2762999285@qq.com>
2023-07-24 14:41:25 +0800

    文件上传的前端封装类

---
+++ src/main/java/com/dachuang/entity/file/dto/FileUploadDTO.java
@@ -0,0 +1,24 @@
+package com.dachuang.entity.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileUploadDTO {
+    /**
+     * 文件
+     */
+   private MultipartFile file;
+    /**
+     * useid
+     */
+    private Long userId;
+}
